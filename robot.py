d225e77fae59d8d94a31fb5d47c8487d5d829693
Kevin Haninger <kevin@haninger>
2023-08-21 18:36:08 +0200

    first rollout fn, 43 us to rollout a length of 4, about 4x faster

--- robot.py
+++ robot.py
@@ -17,6 +17,9 @@ class DynSys():
     def get_input(self, H=1):
         return self._input.extend_vars(H)
 
+    def get_param(self):
+        return self._param
+    
     def get_state(self, H=1):
         return self._state if H==1 else self._state.extend_vars(H)
 
@@ -48,6 +51,7 @@ class Robot(DynSys):
         self.nq, self.fwd_kin, self.mass_fn = load_urdf(urdf_path, ee_frame_name)
         self.build_vars(attrs, visc_fric)
         self.build_fwd_kin(ee_frame_name)
+        if ctrl: self.build_ctrl()
     
     def build_vars(self, attrs, visc_fric):
         """ Build symbolic variables with attributes """
@@ -68,7 +72,9 @@ class Robot(DynSys):
         self._param = ParamSet(init = init)
 
         self._input = DecisionVarSet(['lb', 'ub'])
-        if not self.__ctrl: self._input.add_vars({'tau_input':ca.DM.zeros(self.nq)})
+        if not self.__ctrl:
+            self._input.add_vars({'tau_input':ca.DM.zeros(self.nq)})
+            self.nu = self.nq
         
     def build_inv_mass(self, step_size):
         M = self.mass_fn(self._state['q']) + 0.5*ca.DM.eye(self.nq)
@@ -93,7 +99,6 @@ class Robot(DynSys):
             IN: step_size, time step length in seconds
         """
         self.build_inv_mass(step_size)
-        if self.__ctrl: self.build_ctrl()
         
         # Shorthand
         q = self._state['q']
@@ -101,27 +106,42 @@ class Robot(DynSys):
         inp_args = self._state.get_sym()
         inp_args.update(self._param.get_sym())
         inp_args.update(self._input.get_sym())
-        
+                
         self.tau_ext = self.jac(q).T@self.get_F_ext(q, dq)
 
-        cost = 0 #TODO
+        cost = ca.sumsqr(dq)
         
         # Joint acceleration, then integrate
         ddq = inp_args['M_inv']@(-self.visc_fric@dq + self.tau_ext + self._input['tau_input'])
         dq_next = dq + step_size*ddq
         q_next = q + step_size*dq_next
+        
         self.step = ca.Function('step', inp_args.values(), [q_next, dq_next, cost],
                                         inp_args.keys(), ['q', 'dq', 'cost']).expand()
 
         self._xi = self._state.vectorize()
         inp_args = self._input.get_sym()
         inp_args.update(self._param.get_sym())
-
+        
         self.step_vec = ca.Function('step_vec',
                                     [self._xi, *inp_args.values()],
                                     [ca.vertcat(q_next, dq_next, self._xi[2*self.nq:]), cost],
                                     ['xi', *inp_args.keys()],
                                     ['xi', 'cost'])
+
+    def build_rollout(self, H):
+        xi0 = ca.SX.sym('xi', self.nx)
+        xi = xi0
+        imp_rest  = ca.SX.sym('imp_rest', self.nu, H)
+        imp_stiff = ca.SX.sym('imp_stiff', 3)
+        M_inv =  self.inv_mass_fn(xi[:self.nq])
+        cost = 0
+        for h in range(H):
+            res = self.step_vec(xi = xi, imp_rest = imp_rest[:,h],
+                                M_inv = M_inv, imp_stiff = imp_stiff )
+            cost += res['cost']
+            xi = res['xi']
+        self.rollout = ca.Function('rollout', [xi0, imp_rest, imp_stiff], [cost]).expand() 
         
     def build_linearized(self):
         inp_args = self._input.get_sym()
@@ -147,13 +167,16 @@ class Robot(DynSys):
         print(f'  Building controller {self.__ctrl.name}')
         q = self._state['q']
         dq = self._state['dq']
-        arg_dict = self.__ctrl.get_state().get_sym()
+        arg_dict = self.__ctrl.get_input().get_sym()
+        arg_dict.update(self.__ctrl.get_param().get_sym())
         arg_dict['p'], arg_dict['R'] = self.fwd_kin(q)
         arg_dict['dx'] = self.tcp_motion(q, dq)[1]
-
-        self._input += self.__ctrl.get_state()
         self._input['tau_input'] = self.jac(q).T@self.__ctrl.get_force(arg_dict)
-    
+
+        self._param += self.__ctrl.get_param()
+        self._input += self.__ctrl.get_input()
+        self.nu = self._input.vectorize().shape[0]
+        
     # Returns the force on the TCP expressed in world coordinates
     def get_F_ext(self, q, dq):
         F_ext = ca.DM.zeros(3)
